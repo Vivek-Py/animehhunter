be81fa18831f6a34f46b1bd06927f0afca8fc801
Vivek <snapstar78@gmail.com>
2021-07-10 07:48:46 +0530

    major: player has been added

--- src/App.js
+++ src/App.js
@@ -3,85 +3,110 @@ import "./App.css";
 import SearchIcon from "@material-ui/icons/Search";
 import Movie from "./components/Movie";
 import Swipeable from "./components/Swipeable";
+import ReactPlayer from "react-player";
+
+/* function addVideoModal() {
+	let testData = document.getElementById("video-modal");
+  
+} */
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [status, setStatus] = useState("1");
-  const [searchTerm, setSearchTerm] = useState("");
+	const [movies, setMovies] = useState([]);
+	const [status, setStatus] = useState("1");
+	const [searchTerm, setSearchTerm] = useState("");
+	const [Playback, setPlayback] = useState(false);
 
-  useEffect(() => {
-    let url = "https://jikan1.p.rapidapi.com/genre/anime/" + status + "/1";
-    console.log(url);
-    fetch(url, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": "1009e66191mshe5e97aee1afe149p1bbb93jsne196af4902ac",
-        "x-rapidapi-host": "jikan1.p.rapidapi.com",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setMovies(data.anime));
-  }, [status]);
+	useEffect(() => {
+		let url = "https://jikan1.p.rapidapi.com/genre/anime/" + status + "/1";
+		console.log(url);
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"x-rapidapi-key": "1009e66191mshe5e97aee1afe149p1bbb93jsne196af4902ac",
+				"x-rapidapi-host": "jikan1.p.rapidapi.com",
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => setMovies(data.anime));
+	}, [status]);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
 
-  function searchMovie() {
-    let url = "https://jikan1.p.rapidapi.com/search/anime?q=" + searchTerm;
-    fetch(url, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": "1009e66191mshe5e97aee1afe149p1bbb93jsne196af4902ac",
-        "x-rapidapi-host": "jikan1.p.rapidapi.com",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setMovies(data.results));
+	function searchMovie() {
+		let url = "https://jikan1.p.rapidapi.com/search/anime?q=" + searchTerm;
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"x-rapidapi-key": "1009e66191mshe5e97aee1afe149p1bbb93jsne196af4902ac",
+				"x-rapidapi-host": "jikan1.p.rapidapi.com",
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => setMovies(data.results));
 
-    setSearchTerm("");
-  }
+		setSearchTerm("");
+	}
 
-  return (
-    <div className="root-container">
-      <header>
-        <Swipeable setStatus={setStatus} />
-        <navbrand
-          className="appName"
-          onClick={() => {
-            setStatus("1");
-          }}
-        >
-          aNIME hUNTER
-        </navbrand>
-        <form className="search-form" onSubmit={handleSubmit}>
-          <input
-            type="search"
-            className="search"
-            placeholder="Search..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button type="submit" onClick={searchMovie} className="searchBtn">
-            <SearchIcon className="searchIcon" />
-          </button>
-        </form>
-      </header>
+	return (
+		<div className="root-container">
+			<header>
+				<Swipeable setStatus={setStatus} />
+				<navbrand
+					className="appName"
+					onClick={() => {
+						setStatus("1");
+					}}
+				>
+					aNIME hUNTER
+				</navbrand>
+				<form className="search-form" onSubmit={handleSubmit}>
+					<input
+						type="search"
+						className="search"
+						placeholder="Search..."
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+					<button type="submit" onClick={searchMovie} className="searchBtn">
+						<SearchIcon className="searchIcon" />
+					</button>
+				</form>
+			</header>
 
-      <div className="movie-container">
-        {movies &&
-          movies.map((movie) => (
-            <Movie
-              key={movie.rank}
-              title={movie.title}
-              poster_path={movie.image_url}
-              vote_average={movie.score}
-              overview={movie.synopsis}
-            />
-          ))}
-      </div>
-    </div>
-  );
+			<div id="video-modal" className="video-modal">
+				{Playback === true ? (
+					<>
+						<ReactPlayer url="https://www.youtube.com/watch?v=kDaC3RNurvA" />
+						<button
+							onClick={() => {
+								setPlayback(false);
+							}}
+						>
+							Close
+						</button>
+					</>
+				) : (
+					""
+				)}
+			</div>
+
+			<div className="movie-container">
+				{movies &&
+					movies.map((movie) => (
+						<Movie
+							key={movie.rank}
+							title={movie.title}
+							poster_path={movie.image_url}
+							vote_average={movie.score}
+							overview={movie.synopsis}
+							setPlayback={setPlayback}
+						/>
+					))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,38 @@
 import React from "react";
 
 const Movie = (props) => {
-  const { title, poster_path, vote_average, overview } = props;
+	const { title, poster_path, vote_average, overview, setPlayback } = props;
 
-  function setVoteColor(vote) {
-    if (vote >= 8) {
-      return "green";
-    } else if (vote >= 6) {
-      return "orange";
-    } else {
-      return "red";
-    }
-  }
+	function setVoteColor(vote) {
+		if (vote >= 8) {
+			return "green";
+		} else if (vote >= 6) {
+			return "orange";
+		} else {
+			return "red";
+		}
+	}
 
-  return (
-    <div className="movie">
-      <img src={poster_path} alt="Poster of the anime." />
-      <div className="movie-info">
-        <h3>{title}</h3>
-        <span className={`tag ${setVoteColor(vote_average)}`}>
-          {vote_average}
-        </span>
-      </div>
-      <div className="movie-overview">
-        <h2>Overview: </h2>
-        <p>{overview}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="movie">
+			<img src={poster_path} alt="Poster of the anime." />
+			<div className="movie-info">
+				<h3>{title}</h3>
+				<span className={`tag ${setVoteColor(vote_average)}`}>{vote_average}</span>
+			</div>
+			<div className="movie-overview">
+				<h2>Overview: </h2>
+				<button
+					onClick={() => {
+						setPlayback(true);
+					}}
+				>
+					Watch Trailer
+				</button>
+				<p>{overview}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Movie;
